120080c992f54542d3750341ccdb642e979411c0
Pedro Kawano <pedrokawano@outlook.com>
2024-03-17 22:13:10 -0300

    adiciona leitura

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Blog.Data;
 using Blog.Models;
 
@@ -10,12 +11,33 @@ namespace Blog
         {
             using (var context = new BlogDataContext())
             {
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-                tag.Name = ".NET";
-                tag.Slug = "dotnet";
+                // var tag = new Tag { Name = ".NET", Slug = "dotnet" };
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
 
-                context.Update(tag);
-                context.SaveChanges();
+                // var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                // context.Tags.Add(tag2);
+                // context.SaveChanges();
+
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // context.Remove(tag);
+                // context.SaveChanges();
+
+                var tags = context.Tags
+                    .Where(x => x.Name.Contains(".NET"))
+                    .ToList();
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
             }
         }
     }
